5bb4c8912d4035f5c12a4bfd4a62b2776b0fbe5d
Shivanand <shivanandreddy1@gmail.com>
2026-09-23 12:41:14 +0530

    updated

--- src/pages/billing/BillDetails.jsx
+++ src/pages/billing/BillDetails.jsx
@@ -51,9 +51,9 @@ Invoice   :    ${bill.billNumber}
 Date      :    ${new Date(bill.createdAt).toLocaleDateString("en-GB").replace(/\//g, "-")}
 Customer  :    ${bill.customerName || "Walk-in Customer"}
 Phone     :   ${bill.customerPhone || "-"}
-----------------------
+----------------------------
 ITEM NAME    QTY    TOTAL (₹)
-----------------------
+----------------------------
 ${bill.items
   .map(
     (item) =>
