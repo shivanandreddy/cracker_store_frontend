5b8cb166d30af319794b781133f24cd140757e55
Shivanand <shivanandreddy1@gmail.com>
2026-09-23 11:53:11 +0530

    updated for mobile template

--- src/pages/billing/BillDetails.jsx
+++ src/pages/billing/BillDetails.jsx
@@ -61,16 +61,16 @@ ${bill.items
   .trim()
   )
   .join("\n")}
--------------------------------------
+--------------------------------
 Subtotal                   : ₹${Number(bill.subtotal).toFixed(2)}
 Discount                   : -₹${Number(bill.discount || 0).toFixed(2)}
 GRAND TOTAL                : ₹${Number(bill.grandTotal).toFixed(2)}
--------------------------------------
+--------------------------------
 Payment Mode: ${bill.paymentMethod.toUpperCase()}
-=====================================
+================================
         Thank you for your visit!        
             Please Visit Again             
-=====================================
+================================
 \`\`\``;
 
     const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(
